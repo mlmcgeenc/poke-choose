d72dd54d7f6584d79d4fcd23aa8b3f798fb24968
Matthew McGee <mlmcgeenc@gmail.com>
2024-01-06 11:33:56 -0500

    move filter and display functions to utils folder

--- src/components/PokeButton.tsx
+++ src/components/PokeButton.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 
 const PokeButton: FC<{ pokeType: string }> = ({ pokeType }) => {
-	console.log("Pokebutton props: ", pokeType);
 	return (
 		<>
 			<button className='max-w-1/4 min-w-16 min-h-16 bg-slate-200 m-2 rounded-full'>
