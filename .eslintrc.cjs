5e1ef9682692a13c3401b413552ebb35d6c01c8d
Matthew McGee <mlmcgeenc@gmail.com>
2024-01-09 09:42:03 -0500

    correct eslint config to resolve react-in-scope errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		'simple-import-sort/imports': 'error',
 		'simple-import-sort/exports': 'error',
+    'react/jsx-uses-react': 'off',
 		'react/react-in-jsx-scope': 'off',
 		quotes: ['error', 'single'],
 	},
