e49924df9f6f79b2f6fd4c1bf26e1360728a89da
Matthew McGee <mlmcgeenc@gmail.com>
2024-01-12 10:01:30 -0500

    correct display errors on large screens

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -2,7 +2,7 @@ import { LabelType } from '../types';
 
 const Label = ({ pokemonType }: LabelType) => {
 	return (
-		<div className='flex justify-center items-center text-white font-bold capitalize text-md  mt-2 min-h-1/6'>
+		<div className='flex justify-center items-center text-white font-bold capitalize text-md mt-2 min-w-16 min-h-1/6'>
 			{pokemonType}
 		</div>
 	);
--- src/components/ResultsSheet.tsx
+++ src/components/ResultsSheet.tsx
@@ -20,14 +20,14 @@ const ResultsSheet = ({
 		<div
 			className={`${
 				showResults ? '' : 'translate-y-full ease-in'
-			} bg-ui-blue-darker/80 fixed transform transition-all top-0 w-full duration-200 z-10 h-screen flex flex-col`}
+			} bg-ui-blue-darker/80 fixed transform transition-all top-0 left-0 w-full duration-200 z-10 h-screen flex flex-col`}
 		>
 			<CurrentOpponent
 				opponentType={currentOpponent.type}
 				showResults={showResults}
 				handleToggleShowResults={handleToggleShowResults}
 			/>
-			<div className='max-w-[960px] w-full m-auto flex flex-col p-4 overflow-y-auto grow'>
+			<div className='max-w-[960px] m-auto w-full flex flex-col p-4 overflow-y-auto grow'>
 				<div className='h-1/2'>
 					<TopPicks
 						header='Top Pick'
