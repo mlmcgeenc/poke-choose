35a4def32ffadbc15b0acebf4e8dc49ea610127d
Matthew McGee <mlmcgeenc@gmail.com>
2024-01-12 09:42:36 -0500

    adjust styling for header and labels

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import Logo from '../assets/logo.png';
 const Header = () => {
 	return (
-		<div className='bg-ui-blue-darker w-full p-2'>
+		<div className='bg-ui-blue-darker w-full p-2 border-b border-ui-blue-lighter'>
 			<img className='m-auto px-8' src={Logo} />
 			<h3 className='text-white mt-2'>Choose an opponent type</h3>
 		</div>
