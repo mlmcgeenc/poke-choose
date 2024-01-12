53d0ccdab8b6d86f63710884ff0afb6f82723aa5
Matthew McGee <mlmcgeenc@gmail.com>
2024-01-11 22:42:05 -0500

    remove console

--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -8,7 +8,6 @@ const ResultRow = ({
 	currentOpponent,
 	condition,
 }: DisplayRowType) => {
-	console.log(typeof icon);
 	return (
 		<div className='h-1/2 m-auto w-full flex flex-col my-1 p-2 rounded-md border-2 border-ui-blue-darker bg-ui-blue-darker/90'>
 			<h3 className='text-white font-bold text-left'>
